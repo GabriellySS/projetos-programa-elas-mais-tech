ed97c74b32c9a8be499f8f4fce1ceff0407eeeca
Gabrielly Santos <gabriellysantos@live.com>
2026-02-02 16:18:12 -0300

    adiciona a api key

--- current-weather-ts/assets/ts/index.ts
+++ current-weather-ts/assets/ts/index.ts
@@ -15,7 +15,7 @@ form?.addEventListener('submit', async (event) => {
         return
     }
 
-    const apiKey = "__OPENWEATHER_API_KEY__"
+    const apiKey = "27b62474375505aa254baf5c7ce0de85"
 
     try {
       const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric&lang=pt_br`)
